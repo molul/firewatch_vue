829af59c2db22aa445308ab05c80243a5b46281f
molul <luismorcillo@gmail.com>
2023-10-26 04:46:13 +0200

    Change in navigation buttons

--- src/components/NavigationButtons.vue
+++ src/components/NavigationButtons.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="flex justify-center gap-4 my-4">
-    <NavigationButton @callback="$emit('decrease-page', 100)" label="-100" />
+  <div class="flex justify-center gap-2 my-4">
+    <NavigationButton @callback="$emit('decrease-page', 50)" label="-50" />
     <NavigationButton @callback="$emit('decrease-page', 10)" label="-10" />
-    <NavigationButton @callback="$emit('decrease-page', 5)" label="-5" />
-    <NavigationButton @callback="$emit('decrease-page', 1)" label="Anterior" />
-    <NavigationButton @callback="$emit('increase-page', 1)" label="Siguiente" />
-    <NavigationButton @callback="$emit('increase-page', 5)" label="+5" />
+    <NavigationButton @callback="$emit('decrease-page', 1)" label="<" />
+    <NavigationButton @callback="$emit('increase-page', 1)" label=">" />
     <NavigationButton @callback="$emit('increase-page', 10)" label="+10" />
-    <NavigationButton @callback="$emit('increase-page', 100)" label="+100" />
+    <NavigationButton @callback="$emit('increase-page', 50)" label="+50" />
   </div>
 </template>
 
