9ec5c4afb5ac80dd285751b63812f0a5f7c189c1
molul <luismorcillo@gmail.com>
2023-10-29 11:20:41 +0100

    Header is now responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,9 +8,11 @@ export default {
 
 <template>
   <div
-    class="flex flex-col bg-secondary items-start shadow-xl px-4 py-4 flex-wrap"
+    class="flex flex-col md:flex-row bg-secondary items-center md:items-center shadow-xl p-4 flex-wrap space-y-8 md:space-y-0 md:space-x-4 md:justify-between"
   >
-    <div class="flex flex-col justify-start items-start">
+    <div
+      class="flex md:flex-row flex-col justify-start items-center md:items-center space-y-4 md:space-y-0 md:space-x-4"
+    >
       <div>
         <img
           src="../assets/logo_cesefor.webp"
@@ -19,7 +21,9 @@ export default {
         />
       </div>
       <div class="break-words">
-        <h1 class="text-xl font-bold text-primary break-words">
+        <h1
+          class="text-2xl md:text-2xl lg:text-3xl font-bold text-primary break-words text-center md:text-left"
+        >
           PRUEBA LUIS MORCILLO LUQUE
         </h1>
       </div>
